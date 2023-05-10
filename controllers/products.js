588c27f0c5f982a82b93d44ddc67a3f3eb07abce
Adityaweb3 <ap7596112@gmail.com>
2023-05-11 00:47:25 +0530

    update code

---
+++ controllers/products.js
@@ -0,0 +1,13 @@
+
+
+const getAllProductsStatic =async(req,res)=>{
+    res.status(200).json({msg : 'Product Testing Route'})
+}
+
+const getAllProducts =async(req,res)=>{
+    res.status(200).json({msg : 'Product Route'})
+}
+
+module.exports = {
+    getAllProducts, getAllProductsStatic
+}
